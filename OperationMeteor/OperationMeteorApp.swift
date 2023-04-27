68d06c1e432e6849bdcb1a37577e7cce270f967d
Richard Crichlow <dementedcactus@gmail.com>
2023-04-27 15:32:43 -0500

    Initial Commit

---
+++ OperationMeteor/OperationMeteorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperationMeteorApp.swift
+//  OperationMeteor
+//
+//  Created by Richard Crichlow on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct OperationMeteorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
