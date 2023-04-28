dea0cf6db5fa942fde4e5376962b27af41326e5e
Richard Crichlow <dementedcactus@gmail.com>
2023-04-28 16:08:24 -0500

    HomeView draft completed

--- OperationMeteor/Views/HomeView.swift
+++ OperationMeteor/Views/HomeView.swift
@@ -8,12 +8,112 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        VStack {
-            NavigationLink("Volunteer Sign Up", value: NavigationPath.volunteerSignUp)
-            // Add other NavigationLinks for other views
+        ZStack {
+            Color(.sRGB, red: 0.9, green: 0.9, blue: 0.9, opacity: 0.3)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                NavigationLink("Volunteer Sign Up", value: NavigationPath.volunteerSignUp)
+                // Add other NavigationLinks for other views
+                
+                Text("Welcome to Operation Meteor!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Explore our upcoming projects:")
+                    .font(.headline)
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(alignment: .leading, spacing: 15) {
+                        ForEach(upcomingProjects, id: \.self) { project in
+                            Text(project)
+                                .font(.body)
+                                .padding(.leading, 10)
+                        }
+                    }
+                }
+            }
+            .padding()
         }
     }
+    
+    let upcomingProjects = [
+            "MeteorHealth",
+            "MeteorEducation",
+            "MeteorFinance",
+            "MeteorShop",
+            "MeteorRemote",
+            "MeteorFreelance",
+            "MeteorTravel",
+            "MeteorLanguage",
+            "MeteorCyber",
+            "MeteorMarket",
+            "PantherEco",
+            "PantherJustice",
+            "PantherHomes",
+            "PantherRights",
+            "PantherEnergy",
+            "PantherGig",
+            "PantherTalent",
+            "PantherMentor",
+            "PantherFood",
+            "PantherWellness",
+            "PantherSpace",
+            "PantherCrowd",
+            "PantherConnect",
+            "PantherVoice",
+            "PantherParent",
+            "PantherHobby",
+            "PantherRepair",
+            "PantherStudy",
+            "PantherSelfCare",
+            "PantherGreen",
+            "PantherCharity",
+            "PantherFashion",
+            "PantherRecovery",
+            "PantherCook",
+            "PantherSkill",
+            "PantherNeighbor",
+            "PantherGrowth",
+            "PantherAdventure",
+            "PantherCraft",
+            "PantherUpcycle",
+            "PantherGame",
+            "PantherMusic",
+            "PantherHistory",
+            "PantherPet",
+            "PantherElder",
+            "PantherEvents",
+            "PantherSafety",
+            "PantherCulture",
+            "PantherArts",
+            "PantherSports",
+            "PantherGarden",
+            "PantherVolunteer",
+            "PantherRead",
+            "PantherSpirituality",
+            "PantherFashionTech",
+            "PantherTransport",
+            "PantherInnovate",
+            "PantherAid",
+            "PantherAgriTech",
+            "PantherSmartCity",
+            "PantherSocialImpact",
+            "PantherBioTech",
+            "PantherRobotics",
+            "PantherCleanTech",
+            "PantherVR",
+            "PantherAI",
+            "PantherIoT",
+            "PantherData",
+            "PantherCrypto",
+            "PantherMedTech",
+            "PantherSpaceTech",
+            "PantherQuantum",
+        ]
+    
 }
 
 
--- OperationMeteor/Views/MainTabView.swift
+++ OperationMeteor/Views/MainTabView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MainTabView: View {
     @State private var selectedTab: NavigationPath = .home
-
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             TabBar()
@@ -23,14 +23,41 @@ struct TabBar: View {
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 30) {
                         ForEach(NavigationPath.allCases, id: \.self) { item in
-                            TabItem(title: item.rawValue, path: item)
+                            
+                            HStack {
+                                
+                                Spacer()
+                                
+                                VStack(alignment: .leading) {
+                                    Text(item.rawValue)
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                        .padding(30)
+                                        .lineLimit(4)
+                                    
+                                    TabItem(title: item.rawValue, path: item)
+                                    
+                                    Spacer()
+                                }
+                                .background(Color(.lightGray).opacity(0.2))
+                                .cornerRadius(30)
+                                .shadow(color: Color(.blue).opacity(0.3), radius: 20, x: 0, y: 20)
+                                
+                                Spacer()
+                                
+                            }
+                            .frame(width: 300)
                         }
                     }
                     .padding(.horizontal, 16)
+                    .frame(height: 600)
                 }
+                Spacer()
             }
         }
     }
@@ -39,47 +66,99 @@ struct TabBar: View {
 struct TabItem: View {
     let title: String
     let path: NavigationPath
-
+    
     var body: some View {
         GeometryReader { geometry in
             NavigationLink(destination: view(for: path)) {
                 VStack {
                     view(for: path)
-                        .frame(width: 246, height: 360)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(color: .gray.opacity(0.4), radius: 5, x: 0, y: 5)
-                        .rotation3DEffect(Angle(degrees:
-                            Double(geometry.frame(in: .global).minX - 30) / -40), axis: (x: 0, y: 10.0, z: 0))
+                        .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -40), axis: (x: 0, y: 10.0, z: 0))
                 }
             }
             .padding(.horizontal, 16)
         }
     }
-
+    
+    func colors(for path: NavigationPath) -> (background: Color, shadow: Color) {
+        switch path {
+        case .home:
+            return (.blue, .blue.opacity(0.4))
+        case .volunteerSignUp:
+            return (.orange, .orange.opacity(0.4))
+        case .jobs:
+            return (.purple, .purple.opacity(0.4))
+        case .resources:
+            return (.green, .green.opacity(0.4))
+        case .flyer:
+            return (.red, .red.opacity(0.4))
+        case .localMap:
+            return (.pink, .pink.opacity(0.4))
+        case .leaderboard:
+            return (.yellow, .yellow.opacity(0.4))
+        case .taskMarket:
+            return (.gray, .gray.opacity(0.4))
+        case .communityBoards:
+            return (.teal, .teal.opacity(0.4))
+        case .profile:
+            return (.indigo, .indigo.opacity(0.4))
+        }
+    }
+    
     @ViewBuilder
     func view(for path: NavigationPath) -> some View {
+        let (background, shadow) = colors(for: path)
+        
         switch path {
         case .home:
             HomeView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .volunteerSignUp:
             VolunteerSignUpView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .jobs:
             JobsView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .resources:
             ResourcesView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .flyer:
             FlyerView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .localMap:
             LocalMapView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .leaderboard:
             LeaderboardView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .taskMarket:
             TaskMarketView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .communityBoards:
             CommunityBoardsView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         case .profile:
             ProfileView()
+                .background(background)
+                .cornerRadius(10)
+                .shadow(color: shadow, radius: 5, x: 0, y: 5)
         }
     }
 }
